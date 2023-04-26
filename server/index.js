5063b3a8eab6ed41079797b1e9ebb623f5dd2419
dorothy <amantedorothy@gmail.com>
2023-04-26 19:58:14 +0800

    db update

---
+++ server/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+const PORT = 8000
+
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+    user: process.env.user,
+    host: process.env.host,
+    database: process.env.database,
+    password: process.env.password,
+    port: process.env.port
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`);
+})
+
+app.get('/all-users', (request, response) => {
+    pool.query('SELECT * FROM users', (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows)
+    })
+})
+
+app.get('/get-user/:id', (request, response) => {
+  const id = request.params.id
+
+  pool.query('SELECT * FROM users WHERE users_id = $1', [id], (error, results) =>{
+      if (error) {
+          throw error;
+      }
+      response.status(200).json(results.rows)
+  })
+})
