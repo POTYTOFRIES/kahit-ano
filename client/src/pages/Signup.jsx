4545a6282ef0f661b4c0c0467b536a39b7014ee6
dorothy <amantedorothy@gmail.com>
2023-05-10 13:33:17 +0800

    :rocket: Update

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -144,7 +144,7 @@ function Signup() {
                     value={password}
                     onChange={handleChangePassword}
                   />
-                  {/* <label
+                  <label
                     for="Confirm Password"
                     class="block mb-2 text-sm text-gray-600"
                   ></label>
@@ -154,7 +154,7 @@ function Signup() {
                     id="confirm password"
                     placeholder="Confirm Password"
                     class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 "
-                  /> */}
+                  />
                 </div>
                 
                     
